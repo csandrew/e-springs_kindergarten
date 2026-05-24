315c0a4e496c703c59e0cb469d0132a9f9015833
andreaschemiati@gmail.com <andreaschemiati@gmail.com>
2026-05-24 09:25:24 +0300

    Fix CSS nesting warnings and disable CI warnings for build

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,8 +1,8 @@
-
 module.exports = {
-  plugins: {
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
